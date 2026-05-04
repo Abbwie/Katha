d2d189aa190d25470cf252c425fc2991fcaa91dd
abbwie <juradoabigail2@gmail.com>
2026-05-04 21:12:20 +0800

    added neon db testing #1

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,19 @@
 from fastapi import FastAPI
+from sqlalchemy import create_engine, text
+import os
 
 app = FastAPI()
 
+DATABASE_URL = os.getenv("DATABASE_URL")
+engine = create_engine(DATABASE_URL)
+
 @app.get("/")
-def read_root():
-    return {"ok": True, "message": "FastAPI on Vercel is working again"}
+def root():
+    return {"ok": True}
+
+@app.get("/db-test")
+def db_test():
+    with engine.connect() as conn:
+        result = conn.execute(text("SELECT version()"))
+        version = result.fetchone()[0]
+    return {"postgres_version": version}
