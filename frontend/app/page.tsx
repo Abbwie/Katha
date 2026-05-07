6d867c50abd9bf9b474c5b87be33f451ee83d1a3
khaledmango <140601056+KhaledRagab15@users.noreply.github.com>
2026-05-08 01:14:23 +0800

    Remove Navbar component from page

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -101,7 +101,7 @@ export default function Home() {
 
   return (
     <div className="min-h-screen bg-background">
-      <Navbar />
+      
 
       {/* Hero & Search */}
       <HeroSearch searchQuery={searchQuery} onSearch={setSearchQuery} />
